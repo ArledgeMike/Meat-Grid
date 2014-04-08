0b1556b0092dd190d978e4d64ff3c02199c9aaac
Michael Arledge <arledgemike@gmail.com>
2014-04-08 13:33:34 -0500

    Update solicitation.rb

--- solicitation.rb
+++ solicitation.rb
@@ -14,11 +14,9 @@ end
 @new_file = File.new("final_solicitations.txt", "w")
 
 @a.each do |x|
-@new_file.puts %Q[exec Contrave..AddSolicitationCode @SolicitationCode = '#{x[1]}',]
-@new_file.puts %Q[@SolicitationDescription = '#{x[2]}',]
-@new_file.puts %Q[@MediumDescription = '#{x[4]}',]
-@new_file.puts %Q[@IntendedEntryPath = '#{x[5]}',]
-@new_file.puts %Q[@IntendedDestinationPath = '#{x[6]}',]
-@new_file.puts %Q[@ProductId = 1]
-@new_file.puts "\n\n"
+  @new_file.puts %Q[exec NAME..AddSolicitationCode @SolicitationCode = '#{x[1]}',]
+  @new_file.puts %Q[@variable = '#{x[2]}',]
+  @new_file.puts %Q[@variable = '#{x[4]}',]
+  @new_file.puts %Q[@variable = '#{x[5]}',]
+  @new_file.puts "\n\n"
 end       
